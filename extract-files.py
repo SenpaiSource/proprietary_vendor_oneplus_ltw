e613ca36bcdce68e064de67043770f172ff261fe
SathiyaSenpai <25bai042@hicas.ac.in>
2026-09-09 22:56:17 +0530

    ltw: Initial vendor repo for Microsoft Link to Windows

---
+++ extract-files.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2025 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+module = ExtractUtilsModule(
+    'ltw',
+    'oneplus',
+    device_rel_path='vendor/oneplus/ltw',
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
---
+++ ltw.mk
@@ -0,0 +1,14 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Configs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp_permission_ltw.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp_permission_ltw.xml \
+    $(LOCAL_PATH)/configs/default_grant_permissions_ltw.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/default-permissions/default_grant_permissions_ltw.xml \
+    $(LOCAL_PATH)/configs/oplus-hiddenapi-package-allowlist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/oplus-hiddenapi-package-allowlist.xml \
+    $(LOCAL_PATH)/configs/preinstalled-packages-platform-oplus-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-oplus-product.xml
+
+# Inherit from ltw-vendor.mk
+$(call inherit-product, vendor/oneplus/ltw/ltw-vendor.mk)
