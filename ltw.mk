1473540ee0c12c25e6933b933187f308204297db
SathiyaSenpai <25bai042@hicas.ac.in>
2026-09-13 23:33:08 +0530

    ltw: Enable Settings integration and launcher icon via component-override

--- ltw.mk
+++ ltw.mk
@@ -8,7 +8,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp_permission_ltw.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp_permission_ltw.xml \
     $(LOCAL_PATH)/configs/default_grant_permissions_ltw.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/default-permissions/default_grant_permissions_ltw.xml \
     $(LOCAL_PATH)/configs/oplus-hiddenapi-package-allowlist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/oplus-hiddenapi-package-allowlist.xml \
-    $(LOCAL_PATH)/configs/preinstalled-packages-platform-oplus-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-oplus-product.xml
+    $(LOCAL_PATH)/configs/preinstalled-packages-platform-oplus-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-oplus-product.xml \
+    $(LOCAL_PATH)/configs/component-overrides-ltw.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/component-overrides-ltw.xml
 
 # Inherit from ltw-vendor.mk
 $(call inherit-product, vendor/oneplus/ltw/ltw-vendor.mk)
